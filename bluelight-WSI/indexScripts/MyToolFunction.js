3f4e934bec0b651b4c5cdb85bb1df9da5a2b69a7
JieuTang <jieutang@gmail.com>
2021-09-15 18:08:10 +0800

    新增 Study Class 在 DicomFile Class 之下 並可以取得 Study 的 MetaData

--- bluelight-WSI/indexScripts/MyToolFunction.js
+++ bluelight-WSI/indexScripts/MyToolFunction.js
@@ -2,3 +2,9 @@ function DeepCopy(obj)
 {
     return JSON.parse(JSON.stringify(obj))
 }
+
+function httpAndHttpsReplaceByConfig(url, enableHTTPS)
+{
+    if (enableHTTPS) url = url.replace("http:", "https:");
+    return url;
+}
