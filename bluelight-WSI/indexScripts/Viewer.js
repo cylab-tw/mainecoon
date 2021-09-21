65d731ef449f2795e7529b689b74e1d737b2f08d
JieuTang <jieutang@gmail.com>
2021-09-21 15:54:09 +0800

    可以讀有幾層 Instance 並創造出 Div 了

--- bluelight-WSI/indexScripts/Viewer.js
+++ bluelight-WSI/indexScripts/Viewer.js
@@ -4,5 +4,26 @@ class Viewer
     {
         this.ViewerElementID = ViewerElementID;
         this.DicomFile = DicomFile;
+        this.InstanceDivs = [];
+    }
+
+    init()
+    {
+        //讀有幾層 Instance 並創造出 Div
+        this.CreateInstanceDivs();
+    }
+
+    CreateInstanceDivs()
+    {
+        let Instances = this.DicomFile.Study.Series.Instances;
+        let InstanceLength = Instances.length;
+        
+        for (let i = 0; i < InstanceLength; i++)
+        {
+            let tempInstanceDivID = "InstanceDiv" + i;
+            let tempInstanceDiv = new InstanceDiv(this.ViewerElementID, tempInstanceDivID, Instances[i]);
+            tempInstanceDiv.Create();
+            this.InstanceDivs.push(DeepCopy(tempInstanceDiv));
+        }
     }
 }
