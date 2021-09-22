0393780790a8b2fc3ff9f898b802d1975d621501
JieuTang <jieutang@gmail.com>
2021-09-22 10:37:07 +0800

    新增 Frame 的屬性 Frame Address

--- bluelight-WSI/indexScripts/Frame.js
+++ bluelight-WSI/indexScripts/Frame.js
@@ -1,9 +1,10 @@
 class Frame
-{
-    constructor(Frame_URL, width, height)
+{  
+    constructor(Frame_URL, width, height, FrameAddress)
     {
         this.Frame_URL = Frame_URL;
         this.width = width;
         this.height = height;
+        this.FrameAddress = FrameAddress;
     }
 }
